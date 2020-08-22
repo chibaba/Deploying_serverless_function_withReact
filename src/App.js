29cfac1eab092853ffa8e67cbc2c6652810a2deb
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-22 14:27:43 +0100

    updating the function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import "./App.css";
 
 function App() {
+  // create coins variable set to empty array
+  const [coins, updateCoins] = useState([]);
+  // Define function to all api
+  async function fetchCoins() {
+    const data = await API.get("cryptoapi", "/coins");
+    updateCoins(data.coins);
+  }
+  // call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
 }
