bb7459dd95b6119cf28f376349591d46365245d2
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-08-22 15:31:58 +0100

    fetch coins

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import "./App.css";
 function App() {
   // create coins variable set to empty array
   const [coins, updateCoins] = useState([]);
+  // create additional state to hold user input
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+  // craete a new function to allow user to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
   // Define function to all api
   async function fetchCoins() {
-    const data = await API.get("cryptoapi", "/coins");
+    const { limit, start } = input;
+    const data = await API.get(
+      "cryptoapi",
+      `/coins?limit=${limit}&start=${start}`
+    );
     updateCoins(data.coins);
   }
+  //Add input fields to the UI for the user input
+
   // call fetchCoins function when component loads
   useEffect(() => {
     fetchCoins();
@@ -24,6 +36,17 @@ function App() {
           <h5>${coin.price_usd}</h5>
         </div>
       ))}
+
+      <input
+        onChange={(e) => updateInputValues("limit", e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        placeholder="start"
+        onChange={(e) => updateInputValues("start", e.target.value)}
+      />
+
+      <button onClick={fetchCoins}>Fetch Coins</button>
     </div>
   );
 }
